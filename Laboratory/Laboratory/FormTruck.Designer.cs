6d9347094ae1d847e46102abe419595bfbb312f4
mechtatell <kalachikovia@gmail.com>
2020-10-11 10:57:42 +0400

    final fix

--- Laboratory/Laboratory/FormTruck.Designer.cs
+++ Laboratory/Laboratory/FormTruck.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Laboratory
 {
-    partial class FormTransport
+    partial class FormTruck
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTransport));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTruck));
             this.pictureBoxCar = new System.Windows.Forms.PictureBox();
             this.buttonCreateTruck = new System.Windows.Forms.Button();
             this.buttonLeft = new System.Windows.Forms.Button();
--- Laboratory/Laboratory/FormTruck.cs
+++ Laboratory/Laboratory/FormTruck.cs
@@ -4,11 +4,11 @@ using System.Windows.Forms;
 
 namespace Laboratory
 {
-    public partial class FormTransport : Form
+    public partial class FormTruck : Form
     {
-        private ITransport transport;
+        private ITransport truck;
 
-        public FormTransport()
+        public FormTruck()
         {
             InitializeComponent();
         }
@@ -16,48 +16,45 @@ namespace Laboratory
         private void buttonCreateTruck_Click(object sender, EventArgs e)
         {
             Random random = new Random();
-            transport = new Truck(random.Next(100, 300), random.Next(1000, 2000), Color.GhostWhite);
-            transport.SetPosition(random.Next(10, 100), random.Next(100, 200), pictureBoxCar.Width, pictureBoxCar.Height);
+            truck = new Truck(random.Next(100, 300), random.Next(1000, 2000), Color.GhostWhite);
+            truck.SetPosition(random.Next(10, 100), random.Next(100, 200), pictureBoxCar.Width, pictureBoxCar.Height);
             Render();
         }
 
         private void buttonCreateTanker_Click(object sender, EventArgs e)
         {
             Random random = new Random();
-            transport = new Tanker(random.Next(100, 300), random.Next(1000, 2000), Color.GhostWhite, Color.Red, true, true, true, true);
-            transport.SetPosition(random.Next(10, 100), random.Next(100, 200), pictureBoxCar.Width, pictureBoxCar.Height);
+            truck = new Tanker(random.Next(100, 300), random.Next(1000, 2000), Color.GhostWhite, Color.Red, true, true, true, true);
+            truck.SetPosition(random.Next(10, 100), random.Next(100, 200), pictureBoxCar.Width, pictureBoxCar.Height);
             Render();
         }
 
         private void buttonMove_Click(object sender, EventArgs e)
         {
-            if (transport != null)
-            {
                 string name = (sender as Button).Name;
                 switch (name)
                 {
                     case "buttonUp":
-                        transport.MoveTransport(Direction.Up);
+                        truck?.MoveTransport(Direction.Up);
                         break;
                     case "buttonDown":
-                        transport.MoveTransport(Direction.Down);
+                        truck?.MoveTransport(Direction.Down);
                         break;
                     case "buttonLeft":
-                        transport.MoveTransport(Direction.Left);
+                        truck?.MoveTransport(Direction.Left);
                         break;
                     case "buttonRight":
-                        transport.MoveTransport(Direction.Right);
+                        truck?.MoveTransport(Direction.Right);
                         break;
                 }
                 Render();
-            }
         }
 
         private void Render()
         {
             Bitmap map = new Bitmap(pictureBoxCar.Width, pictureBoxCar.Height);
             Graphics g = Graphics.FromImage(map);
-            transport.Render(g);
+            truck?.Render(g);
             pictureBoxCar.Image = map;
         }
     }
